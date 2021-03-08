fbbca1fc4e6b0cfecbb255163942908da01ec1d3
Ninoslav DJUKIC <ninoslav90@hotmail.com>
2021-03-08 22:35:44 +0100

    Method for distance calculation implemented in DijkstraGraph.cs

--- DijkstraAlgoPath/DijkstraGraph.cs
+++ DijkstraAlgoPath/DijkstraGraph.cs
@@ -7,14 +7,17 @@ namespace DijkstraAlgoPath
 {
     public class DijkstraGraph
     {
-        private readonly Dictionary<string, List<Vertex>> _neighbours = new Dictionary<string, List<Vertex>>();
-        public HashSet<Vertex> VisitedVertices;
-        public List<Vertex> UnvisitedVertices;
+        public List<Vertex> Vertices;
+        private readonly Dictionary<string, List<Vertex>> _neighbours;
+        private readonly HashSet<Vertex> _visitedVertices;
+        private readonly List<Vertex> _unvisitedVertices;
 
         public DijkstraGraph(IEnumerable<Vertex> vertices)
         {
-            VisitedVertices = new HashSet<Vertex>();
-            UnvisitedVertices = new List<Vertex>(vertices);
+            Vertices = vertices.ToList();
+            _visitedVertices = new HashSet<Vertex>();
+            _unvisitedVertices = new List<Vertex>(vertices);
+            _neighbours = new Dictionary<string, List<Vertex>>();
         }
 
         /// <summary>
@@ -31,6 +34,49 @@ namespace DijkstraAlgoPath
                 _neighbours[startingVertexName].Add(vertex);
             else
                 _neighbours.Add(startingVertexName, new List<Vertex> { vertex });
+        }
+
+        /// <summary>
+        /// Calculates the distance between verteces in the graph
+        /// </summary>
+        public void CalculateDistanceBetweenVerteces()
+        {
+            while (_unvisitedVertices.Any())
+            {
+                var smallestDistanceVertex = _unvisitedVertices.OrderBy(x => x.Distance).FirstOrDefault();
+                _unvisitedVertices.Remove(smallestDistanceVertex);
+
+                int skipVerticesNumber = _visitedVertices.Count + 1;
+                int addedEdges = 1;
+                foreach (var vertex in Vertices.Skip(skipVerticesNumber)) // skip visited vertices and add edges to the current selected vertex
+                {
+                    int edgeWeight = addedEdges + 1; // increment edge distance weight by 1
+                    AddEdgeWithWeight(smallestDistanceVertex.Name, vertex.Name, edgeWeight, distance: int.MaxValue);
+                    if (addedEdges == 2) // each edge has two additional edges/paths max
+                        break;
+                    addedEdges += 1;
+                }
+
+
+                var vertexNeighbours = GetNeighboursFor(smallestDistanceVertex.Name);
+                if (vertexNeighbours.Any())
+                {
+                    foreach (var neighbours in vertexNeighbours)
+                    {
+                        var newDistanceFromSource = smallestDistanceVertex.Distance + neighbours.EdgeWeight;
+                        var vertex = Vertices.FirstOrDefault(vertex => vertex.Name.Equals(neighbours.Name));
+                        if (vertex.Distance > newDistanceFromSource)
+                        {
+                            vertex.Distance = newDistanceFromSource;
+
+                            if (smallestDistanceVertex.Name != Vertices.First().Name)
+                                vertex.NumberOfStops += 1;
+                        }
+                    }
+                }
+
+                _visitedVertices.Add(smallestDistanceVertex);
+            }
         }
 
         /// <summary>
--- DijkstraAlgoPath/Program.cs
+++ DijkstraAlgoPath/Program.cs
@@ -22,50 +22,11 @@ namespace DijkstraAlgoPath
             };
 
             var graph = new DijkstraGraph(vertices);
-
-            List<Vertex> visitedVertices = new List<Vertex>();
-            List<Vertex> unvisitedVertices = new List<Vertex>(vertices);
-
-            while (unvisitedVertices.Any())
-            {
-                var smallestDistanceVertex = unvisitedVertices.OrderBy(x => x.Distance).FirstOrDefault();
-                unvisitedVertices.Remove(smallestDistanceVertex);
-
-                int skipVerticesNumber = visitedVertices.Count + 1;
-                int addedEdges = 1;
-                foreach (var vertex in vertices.Skip(skipVerticesNumber)) // skip visited vertices and add edges to the current selected vertex
-                {
-                    int edgeWeight = addedEdges + 1; // increment edge distance weight by 1
-                    graph.AddEdgeWithWeight(smallestDistanceVertex.Name, vertex.Name, edgeWeight, distance: int.MaxValue);
-                    if (addedEdges == 2) // each edge has two additional edges/paths max
-                        break;
-                    addedEdges += 1;
-                }
-
-                
-                var vertexNeighbours = graph.GetNeighboursFor(smallestDistanceVertex.Name);
-                if (vertexNeighbours.Any())
-                {
-                    foreach (var neighbours in vertexNeighbours)
-                    {
-                        var newDistanceFromSource = smallestDistanceVertex.Distance + neighbours.EdgeWeight;
-                        var vertex = vertices.FirstOrDefault(vertex => vertex.Name.Equals(neighbours.Name));
-                        if (vertex.Distance > newDistanceFromSource)
-                        {
-                            vertex.Distance = newDistanceFromSource;
-
-                            if (smallestDistanceVertex.Name != vertices.First().Name)
-                                vertex.NumberOfStops += 1;
-                        }
-                    }
-                }
-
-                visitedVertices.Add(smallestDistanceVertex);
-            }
+            graph.CalculateDistanceBetweenVerteces();
 
             // Write the results in the console
             Console.WriteLine("Vertex\t\t\t Distance from source \t\t Number of stops");
-            foreach (var vertex in vertices)
+            foreach (var vertex in graph.Vertices)
             {
                 Console.WriteLine($"{vertex.Name}\t\t {vertex.Distance}\t\t\t\t {vertex.NumberOfStops}");
             }
